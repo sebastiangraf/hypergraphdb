b6dadb69520c04332a5df4b9965bc8904be74b36
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-01-05 10:57:10 +0100

    fixing CloneMe-Class and the invocation-places

--- src/main/java/org/hypergraphdb/util/CloneMe.java
+++ src/main/java/org/hypergraphdb/util/CloneMe.java
@@ -2,14 +2,14 @@ package org.hypergraphdb.util;
 
 /**
  * <p>
- * Expose the <code>clone</code> method publicly so that it can be invoked
- * at an interface/abstract class level.
+ * Expose the <code>clone</code> method publicly so that it can be invoked at an interface/abstract class
+ * level.
  * </p>
  * 
  * @author Borislav Iordanov
- *
+ * 
  */
-public interface CloneMe extends Cloneable
-{
-    <T> T clone();    
+public interface CloneMe extends Cloneable {
+
+    <T> T cloneMe();
 }
