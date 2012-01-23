d4ffa35ae989731c2449735d7f48ec3f558616d2
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-01-23 11:31:25 +0100

    fixed BergeLink, storing references only locally is not really storing any references now referring to super.outgoing with suitable offsets (but I have to check this)

--- src/main/java/org/hypergraphdb/atom/HGBergeLink.java
+++ src/main/java/org/hypergraphdb/atom/HGBergeLink.java
@@ -16,61 +16,54 @@ import org.hypergraphdb.HGPlainLink;
 /**
  * 
  * <p>
- * A <code>HGBergeLink</code> represent a <em>hyperarc</em> or <em>hyperedge</em> in the 
- * mathematical theory of hypergraphs. A hyperarc has a target set that is partitioned
- * into a <em>head</em> and a <em>tail</em>. The name comes from the presumed inventor of this
- * type of arc, Claude Berge. If it turns out the latter did not actually come up with
- * the definition, too bad for the naming choice :) 
+ * A <code>HGBergeLink</code> represent a <em>hyperarc</em> or
+ * <em>hyperedge</em> in the mathematical theory of hypergraphs. A hyperarc has
+ * a target set that is partitioned into a <em>head</em> and a <em>tail</em>.
+ * The name comes from the presumed inventor of this type of arc, Claude Berge.
+ * If it turns out the latter did not actually come up with the definition, too
+ * bad for the naming choice :)
  * </p>
  * 
  * @author Borislav Iordanov
  */
-public class HGBergeLink extends HGPlainLink
-{
+public class HGBergeLink extends HGPlainLink {
 	private int tailIndex = 0;
-	
-	public HGBergeLink(HGHandle...targets)
-	{
-	    super(targets);
+
+	public HGBergeLink(HGHandle... targets) {
+		super(targets);
 	}
-	
-	public HGBergeLink(int tailIndex, HGHandle...targets)
-	{
+
+	public HGBergeLink(int tailIndex, HGHandle... targets) {
 		super(targets);
 		this.tailIndex = tailIndex;
 	}
-	
-	public HGBergeLink(HGHandle [] head, HGHandle [] tail)
-	{
-		HGHandle [] targets = new HGHandle[head.length + tail.length];
-		System.arraycopy(head, 0, targets, 0, head.length);
-		System.arraycopy(tail, 0, targets, head.length, tail.length);
+
+	public HGBergeLink(HGHandle[] head, HGHandle[] tail) {
+		super.outgoingSet = new HGHandle[head.length + tail.length];
+		System.arraycopy(head, 0, super.outgoingSet, 0, head.length);
+		System.arraycopy(tail, 0, super.outgoingSet, head.length, tail.length);
 		tailIndex = head.length;
 	}
 
-	public Set<HGHandle> getHead()
-	{
+	public Set<HGHandle> getHead() {
 		HashSet<HGHandle> set = new HashSet<HGHandle>();
 		for (int i = 0; i < tailIndex; i++)
 			set.add(getTargetAt(i));
 		return set;
 	}
-	
-	public Set<HGHandle> getTail()
-	{
+
+	public Set<HGHandle> getTail() {
 		HashSet<HGHandle> set = new HashSet<HGHandle>();
 		for (int i = tailIndex; i < getArity(); i++)
 			set.add(getTargetAt(i));
-		return set;		
+		return set;
 	}
-	
-	public int getTailIndex()
-	{
+
+	public int getTailIndex() {
 		return tailIndex;
 	}
 
-	public void setTailIndex(int tailIndex)
-	{
+	public void setTailIndex(int tailIndex) {
 		this.tailIndex = tailIndex;
-	}	
+	}
 }
