ab1e607fed0c0641e6acdfd4f95faae615e7fc70
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-02-08 10:52:02 +0100

    implemented correct hashcode/equals and better toString. Current one was not correct

--- src/main/java/org/hypergraphdb/util/Pair.java
+++ src/main/java/org/hypergraphdb/util/Pair.java
@@ -1,79 +1,82 @@
-/* 
- * This file is part of the HyperGraphDB source distribution. This is copyrighted 
- * software. For permitted uses, licensing options and redistribution, please see  
- * the LicensingInformation file at the root level of the distribution.  
+/*
+ * This file is part of the HyperGraphDB source distribution. This is copyrighted
+ * software. For permitted uses, licensing options and redistribution, please see
+ * the LicensingInformation file at the root level of the distribution.
  * 
- * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved. 
+ * Copyright (c) 2005-2010 Kobrix Software, Inc. All rights reserved.
  */
 package org.hypergraphdb.util;
 
 /**
  * <p>
- * The inexplicably missing class from the <code>java.util</code> package. A generic, immutable <code>Pair</code>
- * class. Hash code computed at construction time and cached.
+ * The inexplicably missing class from the <code>java.util</code> package. A generic, immutable
+ * <code>Pair</code> class. Hash code computed at construction time and cached.
  * </p>
- *  
+ * 
  * @author Borislav Iordanov
  */
-public class Pair<F, S> implements java.io.Serializable 
-{
+public class Pair<F, S> implements java.io.Serializable {
     private static final long serialVersionUID = 6572245056832188590L;
-    
-    int hash;
-	private F first;
-	private S second;
-	
-	public Pair(F first, S second)
-	{
-		this.first = first;
-		this.second = second;		
-	}
-	
-	public F getFirst()
-	{
-		return first;
-	}
-	
-	public S getSecond()
-	{
-		return second;
-	}
-		
-	public int hashCode()
-	{
-	    if (hash == 0)
-	    {
-            hash = (first != null) ? first.hashCode() : 0;
-            if (second != null)
-                hash ^= second.hashCode();      
-	    }	    
-		return hash;
-	}
-	
-	public boolean equals(Object other)
-	{
-		if (! (other instanceof Pair<?,?>))
-			return false;
-		
-		Pair<?,?> p = (Pair<?,?>)other;
-		
-		if (first == null)
-			{ if (p.first != null) return false; }
-		else if (p.first == null)
-			return false;
-		else if (!first.equals(p.first)) 
-			return false;
-		
-		if (second == null) 
-			return p.second == null;
-		else if (p.second == null)
-			return false;
-		else
-			return second.equals(p.second);
-	}
-	
-	public String toString()
-	{
-		return "pair(" + first + "," + second + ")";
-	}
+
+    private F first;
+    private S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pair other = (Pair)obj;
+        if (first == null) {
+            if (other.first != null)
+                return false;
+        } else if (!first.equals(other.first))
+            return false;
+        if (second == null) {
+            if (other.second != null)
+                return false;
+        } else if (!second.equals(other.second))
+            return false;
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "Pair [first=" + first + ", second=" + second + "]";
+    }
+
 }
